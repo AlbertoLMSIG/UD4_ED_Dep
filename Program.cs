920fbfb94906bb206834dc9390c17e2f10b97515
Alberto Marquez <amarmen754@g.educaand.es>
2022-01-13 10:29:42 +0100

    AMM-Primer Comentario

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace get_started_debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
